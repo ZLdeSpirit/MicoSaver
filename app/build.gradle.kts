edc10f58e1815393594b3a05cdb86821d4b2d82c
admin_emcdx6 <masross45@gmail.com>
2026-04-08 10:53:14 +0100

    1.change version 1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.mico.saver.videokeeper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 105
-        versionName = "1.0.5"
+        versionCode = 106
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "${rootProject.name}-v${versionName}-${versionCode}")
